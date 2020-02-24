b0cba2cae0149ddc607946c671c156e9cb6e4368
soitgoes4 <jrr11@geneseo.edu>
2020-02-24 13:04:46 -0500

    Debugged Journal Program and New Journal

--- journal.sh
+++ journal.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-touch ~/OneDrive/Spring 2020/English 340/Journal_$( date '+%Y-%m-%d_%H-%M' ).md
-code ~/OneDrive/Spring 2020/English 340/Journal_$( date '+%Y-%m-%d_%H-%M' ).md
+touch ~/OneDrive/Spring\ 2020/English\ 340/Journal_$( date '+%Y-%m-%d_%H-%M' ).md
+code ~/OneDrive/Spring\ 2020/English\ 340/Journal_$( date '+%Y-%m-%d_%H-%M' ).md
